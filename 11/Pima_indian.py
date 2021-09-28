51fa3783b625252eec2c85f0985d8d3d136bff9e
ys200209 <lys200209@naver.com>
2021-09-28 15:43:38 +0900

    Pima_indian

--- 11/Pima_indian.py
+++ 11/Pima_indian.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns # matplotlib 라이브러리보다 좀 더 정교한 그래프를 그릴게끔 도와주는 seaborn 라이브러리.
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+import tensorflow as tf
 
+'''
 df = pd.read_csv('./dataset/pima-indians-diabetes.csv',
                 names=["pregnant", "plasma", "pressure", "thickness", 
                 "insulin", "BMI", "predigree", "age", "class"])
@@ -36,4 +41,40 @@ sns.heatmap(df.corr(), linewidths=0.1, vmax=0.5, cmap=plt.cm.gist_heat, linecolo
 # cmap: 미리 정해진 matplotlib 색상의 설정값을 불러오는 인자.
 
 plt.show()
+# 이 결과로 plasma(공복 혈당 농도) 항목이 당뇨병 발병에 가장 관련이 있는 것으로 나타났다.
+
+# 따라서 plasma 항목과 당뇨병 발병 항목만 따로 떼어 두 항목 간의 관계를 살펴보면
+grid = sns.FacetGrid(df, col="class")
+grid.map(plt.hist, "plasma", bins=10)
+plt.show()
+# 이 결과로 당뇨병이 발병한 사람의 공복 혈당 농도는 그렇지 않은 사람보다 150~200 의 수치가 매우 높았다.
+'''
+
+# seed 값 생성
+np.random.seed(3)
+tf.random.set_seed(3)
+
+# 데이터 로드
+dataset = np.loadtxt('./dataset/pima-indians-diabetes.csv',
+    delimiter=",")
+
+X = dataset[:, 0:8]
+Y = dataset[:, 8]
+
+# 모델의 설정
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation="relu"))
+model.add(Dense(8, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+# 모델 컴파일
+model.compile(loss="binary_crossentropy",
+                optimizer="adam",
+                metrics=["accuracy"])
+
+# 모델 실행
+model.fit(X, Y, epochs=200, batch_size=10)
+
+# 결과 출력
+print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
 
