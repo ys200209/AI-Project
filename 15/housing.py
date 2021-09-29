9248ca2c4677e869b6d62a927a176d0ff1d14eb9
ys200209 <lys200209@naver.com>
2021-09-29 21:51:19 +0900

    housing

--- 15/housing.py
+++ 15/housing.py
@@ -1,15 +1,39 @@
-import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('./dataset/housing.csv', delim_whitespace=True,
+import numpy
+import pandas as pd
+import tensorflow as tf
+
+# seed 값 설정
+seed = 0
+numpy.random.seed(seed)
+tf.random.set_seed(3)
+
+df = pd.read_csv("./dataset/housing.csv", delim_whitespace=True,
     header=None)
 
-print(df.info())
 
-print("---------------------------------------------------------")
 
-print(df.head())
+dataset = df.values
+X = dataset[:, 0:13]
+Y = dataset[:, 13]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=seed)
 
 model = Sequential()
+model.add(Dense(30, input_dim=13, activation="relu"))
+model.add(Dense(6, activation="relu"))
+model.add(Dense(1)) # 선형함수는 출력층에 활성화함수를 입력하지 않는다.
 
-Y_prediction = model.predict(X_test).flatten() # 데이터 배열이 몇 차원이든 모두 1차원으로 바꿔주는 함수
+model.compile(loss="mean_squared_error", optimizer="adam")
+model.fit(X_train, Y_train, epochs=200, batch_size=10)
 
+# 예측 값과 실제 값의 비교
+Y_prediction = model.predict(X_test).flatten()
+for i in range(10):
+    label = Y_test[i] # 실제값
+    prediction = Y_prediction[i] # 예측값
+    print("실제가격: {:.3f}, 예상가격: {:.3f}".format(label, prediction))
+
+print(df.info())
