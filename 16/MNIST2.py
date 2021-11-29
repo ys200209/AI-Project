ebe353ebe9b7431cd5b33c279181580be502d6fe
ys200209 <lys200209@naver.com>
2021-11-29 23:44:19 +0900

    MNIST Example

---
+++ 16/MNIST2.py
@@ -0,0 +1,6 @@
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from PIL import Image
