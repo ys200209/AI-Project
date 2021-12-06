a0600a3fe0df4a795471dc16ba8c8190e0957788
ys200209 <lys200209@naver.com>
2021-12-06 23:55:55 +0900

    AI Project 1

--- Project/archive/Household_monthly_electricity_bill.py
+++ Project/archive/Household_monthly_electricity_bill.py
@@ -28,40 +28,19 @@ num_children : Number of children in the house
 is_urban : Is the house present in an urban area
 amount_paid : Amount paid as the monthly bill
 '''
-def learning_curve(history, epoch):
-    plt.figure(figsize=(10,5))
-    epoch_range = np.arange(1, epoch + 1)
-
-    # loss 차트
-    plt.subplot(1, 2, 2)
-    plt.plot(epoch_range, history.history['loss'])
-    plt.plot(epoch_range, history.history['val_loss'])
-    plt.title('Model Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel("Loss")
-    plt.legend( ['Train', 'Val']  )
-    plt.show()
-
-    # 정확도 차트  
-    plt.subplot(1, 2, 1)
-    plt.plot(epoch_range, history.history['accuracy'])
-    plt.plot(epoch_range, history.history['val_accuracy'])
-    plt.title('Model Accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel("Accurach")
-    plt.legend( ['Train', 'Val']  )
-    plt.show()
 
     
 
 
 
 data = pd.read_csv('./Project/archive/Household energy bill data.csv')
+'''
 data['people'] = data['num_people'] + data['num_children'] # people 수와 아이들의 수를 합하여 하나의 피쳐로 생성함.
 data['ac_tv'] = data['is_ac']+data['is_tv'] # 에어컨의 여부와 TV의 여부를 합하여 하나의 피쳐로 생성한다.
 data = data.drop(['is_ac','is_tv','num_people','num_children'],axis=1) # 합하여 진 피쳐들을 제거한다.
 
 data = data.drop(['num_rooms', 'housearea', 'is_flat', 'ave_monthly_income'],axis=1)
+'''
 
 seed = 0 # seed 값 설정 (동일한 랜덤값을 추출해주기 위해서 시드값을 설정해준다)
 np.random.seed(seed)
@@ -79,13 +58,6 @@ plt.show()
 '''
 
 
-''' # 그래프로 분포도 나타냄 
-advert_report = sv.analyze(data) 
-advert_report.show_notebook() 
-'''
-
-
-
 X = data.drop(['amount_paid'],axis=1).astype("float")
 Y = data['amount_paid']
 
@@ -94,12 +66,12 @@ Y = data['amount_paid']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=seed)
 
 
-
 model = Sequential()
-model.add(Dense(256, input_dim=3, activation="relu")) # 13개의 input과 30개의 output으로 구성된 입력층 생성
-model.add(Dense(64, activation="relu")) # 50개의 input과 1개의 output으로 구성된 은닉층 생성
-model.add(Dense(64, activation="relu")) 
-model.add(Dense(1)) # 선형회귀는 출력층에 활성화함수를 입력하지 않는다.
+model.add(Dense(256, input_dim=9, activation="relu")) # 13개의 input과 30개의 output으로 구성된 입력층 생성
+model.add(Dense(512, activation="relu")) 
+model.add(Dense(256, activation="relu")) 
+model.add(Dense(1)) 
+
 
 '''
 params = { 
@@ -110,7 +82,7 @@ params = {
 } 
 
 model_Forest = RandomForestRegressor() 
- 
+
 model = RandomizedSearchCV(model, params, scoring='neg_mean_absolute_error') 
  
 ''' 
@@ -118,27 +90,20 @@ model = RandomizedSearchCV(model, params, scoring='neg_mean_absolute_error')
 
 model.compile(loss="mean_absolute_error", optimizer="adam")
 
+model.fit(X_train, Y_train, epochs=600, batch_size=10)
 
-modelpath = "./model/{epoch:02d}-{val_loss:.4f}.hdf5"
-checkpointer = ModelCheckpoint(filepath=modelpath, monitor="val_loss", verbose=0, save_best_only=True)
-early_stopping_callback = EarlyStopping(monitor="val_loss", patience=10)
-
-
-history = model.fit(X_train, Y_train, epochs=200, batch_size=10, validation_data=(X_test, Y_test),
-     callbacks=[early_stopping_callback, checkpointer])
-    
 
 print("X = ", X)
 print("Y = ", Y)
 
 
-print("MSE(Training Data) : ", mean_absolute_error(model.predict(X_test), Y_test))
+print("MAE(Training Data) : ", mean_absolute_error(model.predict(X_test), Y_test))
 
 
 
 print("\n Test Accuracy: %.4f" % (model.evaluate(X_test, Y_test)))
 
-learning_curve(history, 200)
+
 
 
 
